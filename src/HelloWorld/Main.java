348008f046a8076bf60a0680339098a8af859a8b
AlexanderWeismann <a-man-20@hotmail.com>
2020-09-11 15:49:40 -0300

    Completed Part1(using JavaFX) and Part2(using FXML). Descriptions of how my code works is commented in.
    Cheers,
    
    -Alexander Weismann
    #3531638

--- src/HelloWorld/Main.java
+++ src/HelloWorld/Main.java
@@ -1,16 +1,51 @@
 package HelloWorld;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 
 public class Main extends Application {
+
+
+
     @Override
     public void start(Stage primaryStage) {
         //complete application here based on instructions in README.md
 
+        //sets title of window
+        primaryStage.setTitle("Assignment #1");
+
+        //initializes a label and sets its alignment to CENTER
+        Label label = new Label("Default Message");
+        BorderPane.setAlignment(label,Pos.CENTER);
+
+        //initializes a button, tell it to search this class for the event handler, and set its alignment to TOP_LEFT
+        Button button = new Button("Say Hello World!");
+        button.setOnAction(e -> label.setText("Hello 3035!"));
+        BorderPane.setAlignment(button, Pos.TOP_LEFT);
+
+        //initialize the borderpane as "layout" and set its size to 400 x 400
+        BorderPane layout = new BorderPane();
+        layout.setPrefSize(450,500);
+        //Set the position of the button and the label in the borderpane
+        layout.setTop(button);
+        layout.setCenter(label);
+
+        //create a new scene
+        Scene scene = new Scene(layout);
+
+        //set the window to show the scene
+        primaryStage.setScene(scene);
+
+        //show the stage w/ the scene in it
         primaryStage.show();
 
     }
--- src/HelloWorldFXML/Main.java
+++ src/HelloWorldFXML/Main.java
@@ -1,7 +1,7 @@
 package HelloWorldFXML;
 
 import java.io.IOException;
-
+import javafx.scene.Parent;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -12,9 +12,17 @@ import javafx.scene.layout.Pane;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("HelloWorld.fxml"));
         //complete application here based on instructions in README.md
         //use FXML for interface
+
+        //Sets the Window Title to "HelloWorldFXML"
+        primaryStage.setTitle("HelloWorldFXML");
+        //Creates a scene inside the window
+        Scene scene = new Scene(root, 300,300);
+        //sets the scene to be "scene" defined above
+        primaryStage.setScene(scene);
 
         primaryStage.show();
     }
